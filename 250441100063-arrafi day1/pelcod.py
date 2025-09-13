06dd34d81e9cd666cc3f0f7ef5f5f38983f5db11
twiszter <mohilhamarrafi@gmail.com>
2025-09-13 11:44:35 +0700

    250441100063_arrafi_day1

---
+++ 250441100063-arrafi day1/pelcod.py
@@ -0,0 +1,22 @@
+a = 10
+b = 5
+c = a + b 
+d = a - b
+e = a * b
+f = a / b
+g = a % b
+
+print("hasil penjumlahan:", c)
+print("hasil pengurangan:", d)
+print("hasil perkalian:", e)
+print("hasil pembagian:", f)
+print("hasil bagi hasil:", g)
+ 
+
+# nama=input("massukan nama anda: ")
+# umur=input("masukan umur: ")
+# buah=input("masukan buah favorit: ")
+
+# print("nama saya adalah " + nama)
+# print("umur saya adalah" + umur + " tahun")
+# print("buah favorit " + buah)
